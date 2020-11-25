2fc1a943dd98ea79abe766a6f9754b8c8e7b9d74
Martin Langenbacher <martin.langenbacher@telekom.de>
2020-11-25 16:37:33 +0100

    first commit

---
+++ src/main/java/de/langenbachersolar/vokabeltrainer/VokabeltrainerApplication.java
@@ -0,0 +1,13 @@
+package de.langenbachersolar.vokabeltrainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VokabeltrainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VokabeltrainerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/langenbachersolar/vokabeltrainer/VokabeltrainerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.langenbachersolar.vokabeltrainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VokabeltrainerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
